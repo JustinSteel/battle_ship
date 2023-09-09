fcfc37ae55341926a5f647c3bb65ba3f9d487e17
Justin Lee <justinslee201100@yahoo.com>
2023-09-09 16:31:43 -0600

    rewrite placement method

--- lib/board.rb
+++ lib/board.rb
@@ -27,45 +27,20 @@ class Board
   end
 
   def valid_placement?(ship, coordinates)
-    cords_subarray = coordinates.each_slice(1).to_a
-    cords_split = cords_subarray.map do |n|
-      n[0].chars
-    end
-    
-    if ship.length == 2 && coordinates.length == 2 && ((cords_split[0][0] == cords_split[1][0]) && ([cords_split[0][1].ord, cords_split[1][1].ord].each_cons(2).any? { |ordinal1, ordinal2| ordinal2 == ordinal1 + 1 })) || (cords_split[0][1] == cords_split[1][1]) && ([cords_split[0][0].ord, cords_split[1][0].ord].each_cons(2).any? { |ordinal1, ordinal2| ordinal2 == ordinal1 + 1 })
-      return true
-    elsif ship.length == 3 && coordinates.length == 3 && ((cords_split[0][0] == cords_split[1][0] && cords_split[1][0] == cords_split[2][0]) && ([cords_split[0][1].ord, cords_split[1][1].ord, cords_split[2][1].ord].each_cons(2).all? { |ordinal1, ordinal2| ordinal2 == ordinal1 + 1 })) || (cords_split[0][1] == cords_split[1][1] && cords_split[1][1] == cords_split[2][1]) && 
-    ([cords_split[0][0].ord, cords_split[1][0].ord, cords_split[2][0].ord].each_cons(2).all? { |ordinal1, ordinal2| ordinal2 == ordinal1 + 1 })
-      return true
+    if ship.length == coordinates.length
+       helper_method(coordinates)
     else
-      return false
+      false
     end
+  end
+
+  def helper_method(cords)
+    letters = cords.map {|cord| cord[0]}
+    numbers = cords.map {|cord| cord[1..-1].to_i}
+    (letters.uniq.length == 1 && numbers == (numbers.first..numbers.last).to_a) ||
+    (numbers.uniq.length == 1 && ("A"..letters.last).each_cons(cords.count).any? { |each| letters == each}) &&
+    cords.all? {|cell| @cells[cell].empty?}
   end
 end
 
 
-# def valid_placement?(ship, coordinates)
-#   return false if coordinates.length != ship.length || coordinates.uniq.length != ship.length
-#   return false if (coordinates.map {|coordinate| coordinate[0]}.all? {|coordinate| coordinate == coordinates[0][0]} && 
-#   coordinates.map{|coordinate| coordinate[1]}.all? {|coordinate| coordinate == coordinates[0][1]}).false?
-#   # todo consecutive
-#   return false if (coordinate.map {|coordinate| coordinate[0]}.all? {|coordinate| coordinate == [0][0]})
-#   #split coordinate
-
-#   #assign first half of coordinate to ordinal value
-#   #convert ordinal value to range
-#   #Each cons method
-#   true
-# end
-
-# cords_array = coordinates.each_slice(1).to_a
-# cords_split = new.map do |n|
-#   n[0].chars
-# end
-# if coordinates.length == 2 && (cords_split[0][0] == cords_split[0][1] ||
-#   cords_split[1][0] == cords_split[1][1])
-#   return true
-# elsif coordinates.length == 3 && (cords.split[0][0] == cords.split[0][1] == cords.split[0][2] ||
-#   cords.split[1][0] == cords.split[1][1] == cords.split[1][2])
-#   return true
-# end
